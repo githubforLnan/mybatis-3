af0d2285d99a8172b485e11d6e199e93e3526e00
liangnan <liangnan@fanle.com>
2021-07-02 14:29:01 +0800

    编译运行成功！

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-
 mybatis-3 source
 
+idea导入项目后，resourece会变成普通文件，这里需要将resource文件夹设置：右键--> mark directory as -- Resource Root
---
+++ src/test/java/demo/User.java
@@ -0,0 +1,61 @@
+/*
+ *    Copyright 2009-2021 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package demo;
+
+/**
+ * @description:
+ * @author: liangnan
+ * @createDate: 2021/7/1
+ */
+public class User {
+  private String name;
+  private int age;
+  private String des;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getDes() {
+    return des;
+  }
+
+  public void setDes(String des) {
+    this.des = des;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "name='" + name + '\'' +
+      ", age='" + age + '\'' +
+      ", des='" + des + '\'' +
+      '}';
+  }
+
+}
